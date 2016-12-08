52eaf724b2b01dd734d8b486b8572b8021e5a68a
David Martínez <david.martinez@bikomobile.com>
2016-12-08 13:47:20 +0100

    Features: 	* Create base project 	* Include .gitignore file

---
+++ Gourmet/Gourmet/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Gourmet
+//
+//  Created by David Martinez on 08/12/2016.
+//  Copyright © 2016 Atenea. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
