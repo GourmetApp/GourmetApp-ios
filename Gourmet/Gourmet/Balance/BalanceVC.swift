c1beb1555fbef77550e62eced1d3a8906eec63d7
David Martínez <david.martinez@bikomobile.com>
2016-12-08 14:27:11 +0100

    Project: 	* Create main navigation story 	* Create empty controllers for Login and Balance

---
+++ Gourmet/Gourmet/Balance/BalanceVC.swift
@@ -0,0 +1,13 @@
+//
+//  BalanceVC.swift
+//  Gourmet
+//
+//  Created by David Martinez on 08/12/2016.
+//  Copyright © 2016 Atenea. All rights reserved.
+//
+
+import UIKit
+
+class BalanceVC : UIViewController {
+    
+}
---
+++ Gourmet/Gourmet/Login/LoginVC.swift
@@ -0,0 +1,13 @@
+//
+//  LoginVC.swift
+//  Gourmet
+//
+//  Created by David Martinez on 08/12/2016.
+//  Copyright © 2016 Atenea. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC : UIViewController {
+    
+}
